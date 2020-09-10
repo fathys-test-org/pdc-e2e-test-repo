3ae27fc3a138ef81de87c67528eeff8ebf6ebad4
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2020-09-10 09:33:53 +0200

    Commit a vulerability

--- boom.js
+++ boom.js
@@ -1,6 +1,6 @@
 
         // we want the content to have some randomness to be able to push continuously
-        var version = 0.5202285405400771
+        var version = 0.12820014226220788
 
         eval('do some evil stuff')
     
